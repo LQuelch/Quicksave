22444c495138905f8eb48d668c1cd0a058db195f
Leiden Quelch <leiden.quelch@learningpool.com>
2024-02-14 12:44:06 +0000

    setup windows

---
+++ windows/Quicksave/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Quicksave::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Quicksave::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
